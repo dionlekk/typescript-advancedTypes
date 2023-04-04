017a713f65eb39e8720c54ab25eb11f7f6614d04
dionlekk <74821838+dionlekk@users.noreply.github.com>
2023-04-04 16:44:46 +0300

    2 files modified

--- index.ts
+++ index.ts
@@ -25,6 +25,9 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable | Numeric;
 
+//function overload
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
 //Type Guards
 function add(a: Combinable, b: Combinable) {
   if (typeof a === 'string' || typeof b === 'string') {
@@ -32,6 +35,30 @@ function add(a: Combinable, b: Combinable) {
   }
   return a + b;
 }
+
+const result = add('Dion', 'Lekkas');
+
+
+//Optional chaining
+const fetchedUserData = {
+  id: 'u1',
+  name: 'Max',
+  //job: { title: 'CEO', description: 'My own company'}
+};
+//Optional chaining
+console.log(fetchedUserData?.job?.title);  
+
+
+//Nullish coalescing
+const userInput = '';
+const storedData = userInput ?? 'DEAFULT';
+console.log(storedData);
+
+
+
+
+console.log('********************************');
+
 
 type UnknownEmployee = Employee | Admin;
 
@@ -112,3 +139,23 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({type: 'bird', flyingSpeed: 10});
+
+console.log('********************************');
+
+//Type casting
+// version 1 const userInputElement = <HTMLInputElement>document.getElementById('user-input');
+const userInputElement = document.getElementById('user-input')! as HTMLInputElement;
+
+userInputElement.value = 'Hi there!';
+
+//Index properties
+interface ErrorContainer {
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email!',
+  username: 'Must start with a capital character!'
+};
+
+
